b28e3ba13fd74f28974927d85e462270ed321e8d
Delwar Hossain <89420389+dhossainUA@users.noreply.github.com>
2026-03-18 14:04:49 -0500

    MATLAB codes to process data

---
+++ Matlab Codes to process data/EMG_Raw2Process.m
@@ -0,0 +1,232 @@
+%this script load the raw emg signal
+%normalize the signal
+%truncate manually carrot, apple, banana eating signal
+%truncate first five chews of each bite 
+%input: RAW EMG mat file
+%output: Processed signal: EMG_CARROT_SUBxx (contain 10 cell for 10 bites),
+%EMG_APPLE_Subxx (contain 10 cell for 10 bites), EMG_BANANA_Subxx (contain 10 cell for 10 bites)
+
+clc;clear all;close all;
+f=cell2mat(struct2cell(load ('EMG_SUB01.mat')));
+ts = (0:numel(f)-1)/1000;
+figure;
+plot(ts,f);
+title('EMG Response');
+xlabel('Time (Sec)')
+ylabel('EMG response');
+
+%%
+[XNorm, mu, stddev] = featureNormalize(f); %normalize
+
+%truncation of  apple, banana, and carrot (manually)
+%confirm the food order from record
+figure;
+plot(XNorm)
+[x,y]=ginput(6);
+a=XNorm(x(1,1):x(2,1)); %apple
+b=XNorm(x(3,1):x(4,1)); %banana
+c=XNorm(x(5,1):x(6,1)); %carrot
+
+%%
+%truncation of first five chews for first 10 bites of apple
+figure;
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{1}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{2}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{3}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{4}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{5}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{6}=a(x11:x12);
+
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{7}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{8}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{9}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{10}=a(x11:x12);
+
+save('emg_apple_sub01.mat','A') %change filename
+
+%%
+
+%%
+%truncation of first five chews for first 10 bites of carrot
+figure;
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{1}=c(x11:x12);
+
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{2}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{3}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{4}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{5}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{6}=c(x11:x12);
+
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{7}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{8}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{9}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{10}=c(x11:x12);
+save('emg_carrot_sub01.mat','C')
+
+%%
+
+%%
+%truncation of first five chews for first 10 bites of Banana
+figure;
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{1}=b(x11:x12);
+
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{2}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{3}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{4}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{5}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{6}=b(x11:x12);
+
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{7}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{8}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{9}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{10}=b(x11:x12);
+save('emg_banana_sub01.mat','B')
+
+
---
+++ Matlab Codes to process data/STRAIN_Raw2Process.m
@@ -0,0 +1,235 @@
+%this script load the raw emg signal
+%normalize the signal
+%truncate manually carrot, apple, banana eating signal
+%truncate first five chews of each bite 
+%input: RAW EMG mat file
+%output: Processed signal: strain_carrot_subxx (contain 10 cell for 10 bites),
+%strain_apple_subxx (contain 10 cell for 10 bites), strain_banana_subxx (contain 10 cell for 10 bites)
+
+clc;clear all;close all;
+s=cell2mat(struct2cell(load ('STRAIN_SUB01.mat')));
+ts = (0:numel(s)-1)/1000;
+figure;
+plot(ts,s);
+title('PIEZOELECTRIC STRAIN Response');
+xlabel('Time (Sec)')
+ylabel('ADC response');
+
+%% filtering and normalization
+filt=designfilt('lowpassiir', 'PassbandFrequency', 2.7, 'StopbandFrequency', 3, 'PassbandRipple', 1, 'StopbandAttenuation', 60, 'SampleRate', 1000);
+strain=filtfilt(filt,s);
+strain= rmoutliers(strain);
+[XNorm, mu, stddev] = featureNormalize(s); %normalize
+%%
+%truncation of  apple, banana, and carrot (manually)
+%confirm the food order from record
+figure;
+plot(XNorm)
+[x,y]=ginput(6);
+a=XNorm(x(1,1):x(2,1)); %apple
+b=XNorm(x(3,1):x(4,1)); %banana
+c=XNorm(x(5,1):x(6,1)); %carrot
+
+%%
+%truncation of first five chews for first 10 bites of apple
+figure;
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{1}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{2}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{3}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{4}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{5}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{6}=a(x11:x12);
+
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{7}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{8}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{9}=a(x11:x12);
+
+plot(a)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+A{10}=a(x11:x12);
+
+save('strain_apple_sub01.mat','A') %change filename
+
+%%
+
+%%
+%truncation of first five chews for first 10 bites of carrot
+figure;
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{1}=c(x11:x12);
+
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{2}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{3}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{4}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{5}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{6}=c(x11:x12);
+
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{7}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{8}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{9}=c(x11:x12);
+
+plot(c)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+C{10}=c(x11:x12);
+save('strain_carrot_sub01.mat','C')
+
+%%
+
+%%
+%truncation of first five chews for first 10 bites of Banana
+figure;
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{1}=b(x11:x12);
+
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{2}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{3}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{4}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{5}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{6}=b(x11:x12);
+
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{7}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{8}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{9}=b(x11:x12);
+
+plot(b)
+x1=ginput();
+x11=x1(1,1);
+x12=x1(2,1);
+B{10}=b(x11:x12);
+save('strain_banana_sub01.mat','B')
+
+
